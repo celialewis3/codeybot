403d06849277a55aec941bde19390c67a84341d4
Celia <celia.lewis@embark-studios.com>
2021-03-29 21:26:59 -0400

    init

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use serenity::model::prelude::Reaction;
 use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
 use serenity::model::id::*;
-use serenity::model::*;
 use serenity::model::prelude::Member;
 use serenity::model::channel::Message;
 use serenity::framework::standard::{
@@ -26,6 +25,8 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
 
+
+    /* TODO: This currently doesn't work as intended. */
     async fn guild_member_addition(&self, ctx: Context, gid: GuildId, person: Member)
     {
         let mut greeting: String = "welcome to codeyclub, ".to_owned();
@@ -39,67 +40,97 @@ impl EventHandler for Handler {
     }
 
     async fn reaction_add(&self, ctx: Context, reaction: Reaction) {
-        let saying = reaction.emoji.as_data();
-        if let Err(why) = reaction.channel_id.say(&ctx.http, saying).await {
-            println!("Error sending message: {}", why);
-        }
-    }
+
+        /* Codey will add a role to a member given the react used */
+
+        let emoji = reaction.emoji.as_data();
+        let gid = reaction.guild_id.unwrap();
+        let user_id = reaction.user_id.unwrap();
+        let member_result = gid.member(&ctx, user_id).await;
+        let mut member = match member_result {
+            Ok(v) => v,
+            Err(e) => {println!("{}", e); return},
+        };
+
+        let role_to_add = match emoji.as_str() {
+            "🍓" => RoleId(826230179097608202),
+            "\u{1FAD0}" => RoleId(826230344243347466),
+            "\u{1F347}" => RoleId(826245452805963820),
+            _ => return,
+         };
+
+         if let Err(why) = member.add_role(&ctx, role_to_add).await {
+            println!("Error assigning role: {}", why);
+         }
+
+     }
+
+    /* Bot responds to messages */
 
     async fn message(&self, ctx: Context, msg: Message) {
 
-        if msg.content == "!surprise" {
-            let path = vec!["images/surprised.png"];
-            if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
-                println!("Error sending file: {}", why);
-            }
-        }
-
-        if msg.content == "!jam" {
-            let path = vec!["images/pingujam.gif"];
-            if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
-                println!("Error sending file: {}", why);
-            }
-        }
-
-        if msg.content == "!hearts" {
-            let path = vec!["images/hearts.jpg"];
-            if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
-                println!("Error sending file: {}", why);
-            }
-        }
-
-        if msg.content == "!cornjail" {
-            let path = vec!["images/cornjail.png"];
-            if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
-                println!("Error sending file: {}", why);
-            }
-        }
-
-
-        if msg.content == "!heh" {
-            let path = vec!["images/heh.png"];
-            if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
-                println!("Error sending file: {}", why);
-            }
-        }
-
-
-        if msg.content == "!panic" {
-            let path = vec!["images/panic.jpg"];
-            if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
-                println!("Error sending file: {}", why);
-            }
-        }
-
-        if msg.content == "!twitch" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, 
-                "You can find my twitch channel at https://www.twitch.tv/celiacode").await {
-                    println!("Error {}", why);
+        match msg.content.as_str() {
+            "!surprise" => {
+                let path = vec!["images/surprised.png"];
+                if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
+                    println!("Error sending file: {}", why);
                 }
+            },
+        
+            "!jam" => {
+                let path = vec!["images/pingujam.gif"];
+                if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
+                    println!("Error sending file: {}", why);
+                }
+            },
+        
+            "!realizing" => {
+                let path = vec!["images/realization.jpg"];
+                if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
+                    println!("Error sending file: {}", why);
+                }
+            },
+        
+            "!hearts" => {
+                let path = vec!["images/hearts.jpg"];
+                if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
+                    println!("Error sending file: {}", why);
+                }
+            },
+        
+            "!cornjail" => {
+                let path = vec!["images/cornjail.png"];
+                if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
+                    println!("Error sending file: {}", why);
+                }
+            },
+        
+            "!heh" => {
+                let path = vec!["images/heh.png"];
+                if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
+                    println!("Error sending file: {}", why);
+                }
+            },
+        
+            "!panic" => {
+                let path = vec!["images/panic.jpg"];
+                if let Err(why) = msg.channel_id.send_files(&ctx.http, path, |m| m.content("") ).await {
+                    println!("Error sending file: {}", why);
+                }
+            },
+        
+            "!twitch" => {
+                if let Err(why) = msg.channel_id.say(&ctx.http, 
+                    "You can find my twitch channel at https://www.twitch.tv/celiacode").await {
+                        println!("Error {}", why);
+                    }
+            },
+        
+            _ => {},
+        
         }
-    }
 
-    
+    }
 }
 
 #[tokio::main]
